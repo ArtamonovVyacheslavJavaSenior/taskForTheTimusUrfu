50bf1ab149ee028b3888bccfc0d9a9ae9876a6f5
Artamonov Vyacheslav <igrok9636@gmail.com>
2025-11-16 21:37:29 +0500

    Task one

---
+++ src/main/java/ru/tyumen/artamonov/vyacheslav/volume/one/abproblem/Solution.java
@@ -0,0 +1,14 @@
+package ru.tyumen.artamonov.vyacheslav.volume.one.abproblem;
+
+import java.util.Scanner;
+
+public class Solution {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        long numberA = scanner.nextLong();
+        long numberB = scanner.nextLong();
+
+        System.out.println(numberA + numberB);
+    }
+}
